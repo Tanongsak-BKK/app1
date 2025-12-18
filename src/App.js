cec95524c7d38496b5380860cf2d22a512c9bf8d
Tanongsak-BKK <jateva01@gmail.com>
2025-12-18 11:40:39 +0700

    add table to app.js
    add table to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <table border="1">
+      <tr><th><center><b>Product</b></center></th><th><b>Price</b></th></tr>
+      <tr><td>React</td><td>320</td></tr>
+      <tr><td>React Native</td><td>340</td></tr>
+    </table>
   );
 }
 
 export default App;
+
