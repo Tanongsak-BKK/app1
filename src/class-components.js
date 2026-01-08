48243043ed41542d41ab0cc690680dd9d4193460
Tanongsak-BKK <jateva01@gmail.com>
2026-01-08 10:26:00 +0700

    Event Handler
    Problem Event Handler

--- src/class-components.js
+++ src/class-components.js
@@ -1,23 +1,14 @@
 import React,{Component} from 'react';
 
-export default class Calendar extends Component {
-    getDate(){
-        const dayNames = ['วันอาทิตย์','วันจันทร์','วันอังคาร','วันพุธ','วันพฤหัสบดี','วันศุกร์','วันเสาร์'];
-        const monthNames = ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน',
-                            'พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม',
-                            'กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'];
-        const date = new Date();
-        const weekDay = dayNames[date.getDay()];
-        const day = date.getDate();
-        const month = monthNames[date.getMonth()];
-        const year = date.getFullYear()+543;
-        return `${weekDay} ที่ ${day} ${month} พ.ศ. ${year}`;
+export default class Button extends Component {
+    
+    showAlert(msg){
+        alert(msg);
+    }
+    onClickButtonOK = () => {
+        this.showAlert("Hello");
     }
     render(){
-        return(
-            <div style={{textAlign:'center', marginTop:20, fontSize:20, color:'black'}}>
-                {this.getDate()}
-            </div>
-        )
+        return <button onClick={this.onClickButtonOK}>ok</button>
     }
 }
