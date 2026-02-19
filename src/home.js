4d92d8af106144ce9c21c0f196635eb1cae7fcde
BKK <jateva01@gmail.com>
2026-02-19 10:38:34 +0700

    update route

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ export default function Home(){
     return(
         <p style={{textAlign:'center'}}>
                 <h3>ยินดีต้อนรับสู่ React Store</h3>
-                รายชื่อ<a href="/product">สินค้า</a>ที่เรามีจำหน่าย
+                รายชื่อ<a href="/products">สินค้า</a>ที่เรามีจำหน่าย
                 <br/>
                 แต่ท่านต้องเป็น<a href="/member">สมาชิก</a>จึงจะสั่งได้
                 <br/>
